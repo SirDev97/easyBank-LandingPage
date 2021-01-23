a14c59ffc5d6e6ba734bf2449720b892a2cdf926
SirDev97 <alkin971@gmail.com>
2021-01-23 16:42:35 +0100

    Finished Animated Menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function() {
+    // Close menu
+    if (header.classList.contains('open')) { 
+        header.classList.remove('open');
+        body.classList.remove('noscroll');
+        fadeElems.forEach(function(element) {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        })
+    } else { // Open menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
+    }
+});
